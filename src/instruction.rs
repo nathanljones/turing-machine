d251b85f58dc95042159dd92ac90b201def44514
nathanljones <nathan.l.jones+github@protonmail.com>
2025-05-01 22:48:42 +0100

    Add the instruction structures

---
+++ src/instruction.rs
@@ -0,0 +1,23 @@
+use std::collections::HashMap;
+use crate::direction::Direction;
+
+// THis is the instruction key to be used to look up
+// the correct instruction in the hashset
+pub struct InstructionKey{
+    state:char,
+    current_symbol:char,
+}
+// Next instruction to be called
+pub struct Instruction{
+    direction:Direction,
+    write_symbol:char,
+    new_state:char,
+}
+
+pub struct Instructions{
+    instructions: HashMap<InstructionKey,Instruction>,
+}
+// get the next instruction
+pub fn get_next_instruction(instruction_key:InstructionKey) -> HashMap<InstructionKey,Instruction>{
+todo!()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+mod instruction;
+mod direction;
+
 fn main() {
     println!("Hello, world!");
 }
